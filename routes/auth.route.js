fd7f4c7c6b7d9e799ab31c4827dab7ff7352da8a
Mohamed AAROUS <43545861+medaros@users.noreply.github.com>
2019-04-19 15:49:39 +0200

    Update auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,22 +6,21 @@ let User = require('../models/users');
 
 authRoute.post("/login", (req,res) => {
     // checker si l'user et password exist dans la base de donnees
-//     User.findOne({ email: req.body.email, password: req.body.password }, (err, user) => {
-//         // si l'user avec le mot de passe exist 
-//         if(user) {
-//             // envoyer une response avec status et email
-//             res.json({status: "loged", data: req.body.email})
-//             // ca c'est pour nous
-//             console.log("V :", user.email, " : is loged in");
-//         }
-//         else {
-//             // sinon envoyer une response avec l'erreur
-//             res.json({status: "error", data: req.body.email})
-//             // et autre fois ca c'est pour nous
-//             console.log(`X : tentative de se connecter avec ${req.body.email} et ${req.body.password}`)
-//         }
-//     });
-    res.send(req.body)
+    User.findOne({ email: req.body.email, password: req.body.password }, (err, user) => {
+        // si l'user avec le mot de passe exist 
+        if(user) {
+            // envoyer une response avec status et email
+            res.json({status: "loged", data: req.body.email})
+            // ca c'est pour nous
+            console.log("V :", user.email, " : is loged in");
+        }
+        else {
+            // sinon envoyer une response avec l'erreur
+            res.json({status: "error", data: req.body.email})
+            // et autre fois ca c'est pour nous
+            console.log(`X : tentative de se connecter avec ${req.body.email} et ${req.body.password}`)
+        }
+    });
 });
 
 authRoute.post("/register", (req,res) => {
