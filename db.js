856b08b5aa7e5aa6518d58ab46d8b94ea244959e
Mohamed AAROUS <43545861+medaros@users.noreply.github.com>
2019-04-19 15:04:37 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // db.js
 
 module.exports = {
-  DB: 'mongodb+srv://root:s4froot@test-fslk8.mongodb.net/test?retryWrites=true',
+  DB: 'mongodb+srv://root:s4froot@test-fslk8.mongodb.net/S4F?retryWrites=true',
 }
