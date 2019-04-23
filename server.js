cc1d00ed2892ac8cb4af87f3ece43a7a1b78d39e
Mohamed AAROUS <43545861+medaros@users.noreply.github.com>
2019-04-23 17:55:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb+srv://root:s4froot.@s4f-fslk8.mongodb.net/S4F', { useN
 )
 //////// test
 const MongoClient = require(‘mongodb’).MongoClient;
-const uri = "mongodb+srv://root:<password>@s4f-fslk8.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://root:s4froot.@s4f-fslk8.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
