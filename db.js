4596ad1cfa5917f66598cec56342df658662b14d
Mohamed AAROUS <43545861+medaros@users.noreply.github.com>
2019-04-23 16:55:29 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,5 +2,5 @@
 
 module.exports = {
 //   DB: 'mongodb://root:s4froot@s4f-fslk8.mongodb.net/test',
-  DB: 'mongodb://s4f_admin:s4froot@ds145486.mlab.com:45486/heroku_92r8j80x'
+  DB: 'mongodb://s4f_admin:s4froot@ds145486.mlab.com:45486/heroku_92r8j80x&w=1'
 }
